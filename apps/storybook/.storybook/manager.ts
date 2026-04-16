f9ee0c5e7b4c3b3450e5c99afe42b4608d2ed75d
Simon Mayrhofer <simon@sirpauls.com>
2026-04-16 16:14:20 +0200

    fix: remove non-existent @storybook/theming and manager-api packages
    - Storybook 10.3.5 doesn't have separate theming packages
    - Use simple object export in manager.ts for custom branding

--- apps/storybook/.storybook/manager.ts
+++ apps/storybook/.storybook/manager.ts
@@ -1,13 +1,7 @@
-import { create } from 'storybook/theming'
-
-export default create({
+export default {
   base: 'light',
   brandTitle: 'paul',
   brandUrl: 'https://paul.sirpauls.com',
   brandImage: '/logo.png',
   brandTarget: '_self',
-
-  // Colors matching your logo (yellow and blue)
-  colorPrimary: '#3366FF', // Blue from your logo
-  colorSecondary: '#FFFF00', // Yellow from your logo
-})
+}
