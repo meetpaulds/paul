33b90caf527fbf17d93c3bb3c36d4f6bc6221ece
Simon Mayrhofer <simon@sirpauls.com>
2026-04-17 08:05:31 +0200

    fix: use correct Storybook 10 manager-api import path
    - Import from 'storybook/manager-api' instead of '@storybook/manager-api'
    - Use addons.setConfig() with theme object to customize branding

--- apps/storybook/.storybook/manager.ts
+++ apps/storybook/.storybook/manager.ts
@@ -1,7 +1,11 @@
-export default {
-  base: 'light',
-  brandTitle: 'paul',
-  brandUrl: 'https://paul.sirpauls.com',
-  brandImage: '/logo.png',
-  brandTarget: '_self',
-}
+import { addons } from 'storybook/manager-api'
+
+addons.setConfig({
+  theme: {
+    base: 'light',
+    brandTitle: 'paul',
+    brandUrl: 'https://paul.sirpauls.com',
+    brandImage: '/logo.png',
+    brandTarget: '_self',
+  } as any,
+})
